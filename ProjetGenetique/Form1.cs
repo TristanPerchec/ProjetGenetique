151cbc1ccb4ea7bbf1580dc5a416f773db612e15
unknown <vincent.guajioty@epsi.fr>
2016-01-26 20:02:10 +0100

    Interface Utilisateur

--- ProjetGenetique/Form1.cs
+++ ProjetGenetique/Form1.cs
@@ -16,5 +16,25 @@ namespace ProjetGenetique
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
